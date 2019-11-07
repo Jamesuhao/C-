8ae0ce8bc3b7fdcd2a2d07cd2a76df45265b00c5
Jamesuhao <1766727840@qq.com>
2019-11-07 11:31:48 +0800

    练习

--- C++腾讯课堂/C++STL/C++STL.cpp
+++ C++腾讯课堂/C++STL/C++STL.cpp
@@ -31,20 +31,26 @@ greater,less
 sort,find,find_if,binary_search,for_each
 */
 #include<iostream>
-using namespace std;
+
+#include<string>
 #include<vector>
 #include<deque>
 #include<list>
-#include<stack>//stack
-#include<queue>//queue和priority_queue(优先级队列)
+//stack
+#include<stack>
+//queue和priority_queue(优先级队列)
+#include<queue>
 //使用有序关联容器包含的头文件
 #include<set>//set multiset
 #include<map>//map miltimap
 //使用无序关联容器包含的头文件
 #include<unordered_set>
 #include<unordered_map>
-#include<string>
-
+//包含了C++STL里面所有的泛型算法
+#include<algorithm>
+//包含了函数对象和绑定器
+#include<functional>
+using namespace std;
 //1.顺序容器：
 #if 0
 /*
@@ -895,4 +901,79 @@ int main()
 }
 #endif
 
-//6.泛型算法和
+//6.泛型算法和绑定器
+#if 0
+/*
+泛型算法===>template + 迭代器 +函数对象
+sort,find,find_if,binary_search,for_each
+泛型算法的特点：
+1.泛型算法的参数接收的都是迭代器
+2.泛型算法的参数还可以接收函数对象(C函数指针）
+
+
+绑定器 + 二元函数对象 =》一元函数对象
+bind1st：把二元函数对象operator()(a,b)第一个形参绑定起来
+bind2nd：把二元函数对象operator()(a,b)第二个形参绑定起来
+*/
+int main()
+{
+	int arr[] = { 12,4,78,9,21,43,56,52,42,31 };
+	vector < int>vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
+	for (int v : vec)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+	//默认小到大的排序
+	sort(vec.begin(), vec.end());
+	for (int v : vec)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+	//有序容器中进行二分查找 O(log2N)
+	if (binary_search(vec.begin(), vec.end(), 21))
+	{
+		cout<<"binary_search21存在"<<endl;
+	}
+	//遍历 O(N)
+	auto it1 = find(vec.begin(), vec.end(), 21);
+	if (it1 != vec.end())
+	{
+		cout << "find21存在" << endl;
+	}
+	//传入函数对象greater，改变容器元素排序时比较方法
+	sort(vec.begin(), vec.end(), greater<int>());
+	for (int v : vec)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+
+	//78 56 52 43 42 31 21 12 9 4
+	//把48按序插入到vector中，找第一个小于48的数字
+	//find_if需要的是一个一元函数对象
+	//greater a>b  less a<b
+	//auto it2 = find_if(vec.begin(), vec.end(), bind1st(greater<int>(), 48));
+	//auto it2 = find_if(vec.begin(), vec.end(), bind2nd(less<int>(),48));
+	auto it2 = find_if(vec.begin(), vec.end(), [](int val)->bool {return val < 48; });
+	vec.insert(it2, 48);
+	for (int v : vec)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+
+	//for_each可以遍历容器的所有元素，可以自行添加合适的函数对象，对容器的元素进行过滤
+	for_each(vec.begin(), vec.end(),
+		[](int val)->void
+		{
+			if (val % 2 == 0)
+			{
+				cout << val << " ";
+			}
+		});
+	cout << endl;
+	return 0;
+}
+#endif
